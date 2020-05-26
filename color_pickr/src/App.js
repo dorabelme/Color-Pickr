b0b7acccbf5778f089235b86ae7d494cd9646817
Dora Belme <contact.dorabelme@gmail.com>
2020-05-26 15:43:11 -0400

    Link to ColorBox and Route

--- color_pickr/src/App.js
+++ color_pickr/src/App.js
@@ -16,9 +16,12 @@ class App extends Component {
       <Switch>
         <Route exact path="/" render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />} />
         <Route exact path="/palette/:id" render={(routeProps) => (
-          <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
+          <Palette
+            palette={generatePalette(
+              this.findPalette(routeProps.match.params.id))} />
         )}
         />
+        <Route exact path="palette/:paletteId/:colorId" render={() => <h1>SINGLE COLOR PAGE</h1>} />
       </Switch>
 
       // <div>
