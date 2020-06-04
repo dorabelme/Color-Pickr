1fa1b0b7cb6a1d0ec11fe9bad5f7f9c6a849589f
Dora Belme <contact.dorabelme@gmail.com>
2020-06-03 20:07:45 -0400

    finish MiniPalette delete

--- color_pickr/src/App.js
+++ color_pickr/src/App.js
@@ -1,38 +1,59 @@
 import React, { Component } from "react";
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import PaletteList from "./PaletteList";
 import SingleColorPalette from "./SingleColorPalette";
-import NewPaletteForm from "./NewPaletteForm";
 import seedColors from "./seedColors";
-import { generatePalette } from './colorHelpers';
+import NewPaletteForm from "./NewPaletteForm";
+import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
   constructor(props) {
     super(props);
     const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
-    this.state = { palettes: savedPalettes || seedColors }
+    this.state = { palettes: savedPalettes || seedColors };
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
+    this.deletePalette = this.deletePalette.bind(this);
   }
   findPalette(id) {
     return this.state.palettes.find(function (palette) {
       return palette.id === id;
-    })
+    });
   }
-
+  deletePalette(id) {
+    this.setState(
+      st => ({ palettes: st.palettes.filter(palette => palette.id !== id) }),
+      this.syncLocalStorage
+    );
+  }
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] }, this.syncLocalStorage);
+    this.setState(
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
+    );
   }
-
   syncLocalStorage() {
-    window.localStorage.setItem("palettes", JSON.stringify(this.state.palettes))
+    //save palettes to local storage
+    window.localStorage.setItem(
+      "palettes",
+      JSON.stringify(this.state.palettes)
+    );
   }
-
   render() {
     return (
       <Switch>
-        <Route exact path="/palette/new" render={(routeProps) => <NewPaletteForm savePalette={this.savePalette} palettes={this.state.palettes} {...routeProps} />} />
+        <Route
+          exact
+          path='/palette/new'
+          render={routeProps => (
+            <NewPaletteForm
+              savePalette={this.savePalette}
+              palettes={this.state.palettes}
+              {...routeProps}
+            />
+          )}
+        />
         <Route
           exact
           path='/palette/:paletteId/:colorId'
@@ -49,7 +70,11 @@ class App extends Component {
           exact
           path='/'
           render={routeProps => (
-            <PaletteList palettes={this.state.palettes} {...routeProps} />
+            <PaletteList
+              palettes={this.state.palettes}
+              deletePalette={this.deletePalette}
+              {...routeProps}
+            />
           )}
         />
         <Route
