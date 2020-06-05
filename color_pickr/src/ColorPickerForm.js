9d75eadd7a6d5598972cb5285b0f3656482a580d
Dora Belme <contact.dorabelme@gmail.com>
2020-06-05 18:56:26 -0400

    fix issues with PaletteForm

--- color_pickr/src/ColorPickerForm.js
+++ color_pickr/src/ColorPickerForm.js
@@ -59,7 +59,10 @@ class ColorPickerForm extends Component {
                     onChangeComplete={this.updateCurrentColor}
                     className={classes.picker}
                 />
-                <ValidatorForm onSubmit={this.handleSubmit} ref='form'>
+                <ValidatorForm
+                    onSubmit={this.handleSubmit}
+                    ref='form'
+                    instantValidate={false}>
                     <TextValidator
                         value={newColorName}
                         name='newColorName'
--- color_pickr/src/styles/DraggableColorBoxStyles.js
+++ color_pickr/src/styles/DraggableColorBoxStyles.js
@@ -1,4 +1,5 @@
 import sizes from "./sizes";
+import chroma from "chroma-js";
 
 const styles = {
     root: {
@@ -24,7 +25,7 @@ const styles = {
         [sizes.down("sm")]: {
             width: "100%",
             height: "5%"
-        },
+        }
     },
     boxContent: {
         position: "absolute",
@@ -32,7 +33,10 @@ const styles = {
         left: "0px",
         bottom: "0px",
         padding: "10px",
-        color: "rgba(0,0,0,0.5)",
+        color: props =>
+            chroma(props.color).luminance() <= 0.08
+                ? "rgba(255,255,255,0.8)"
+                : "rgba(0,0,0,0.6)",
         letterSpacing: "1px",
         textTransform: "uppercase",
         fontSize: "12px",
@@ -42,6 +46,6 @@ const styles = {
     deleteIcon: {
         transition: "all 0.3s ease-in-out"
     }
-}
+};
 
 export default styles;
